dce4559b4a588b7d904f7f6f6a6a3b7388a479e4
Claire Prevost <72880999+clairette22@users.noreply.github.com>
2024-05-29 19:37:12 +0200

    add main

---
+++ main.py
@@ -0,0 +1,60 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from a_pretraitement import process_and_straighten_image
+from b_segmentation_zones import segmentation_region
+from c_segmentation_lignes import separe_en_lignes
+from d_segmentation_caracteres import separe_en_caracteres
+# from e_reconnaissance_caract import 
+import os
+
+
+# 1 - Prétraitement
+nom_image = 'image'
+
+if not os.path.isfile(f'Test_folder/1_{nom_image}_traitee_redressee.jpg'):
+    input_image_path = f'Test_folder/{nom_image}.jpg'
+    output_image_path = f'Test_folder/1_{nom_image}_traitee_redressee.jpg'
+    process_and_straighten_image(input_image_path, output_image_path)
+
+# 2 - Segmentation zones
+if not os.path.exists('Test_folder/regions'):
+    segmentation_region(f'Test_folder/1_{nom_image}_traitee_redressee.jpg')
+
+# 3 - Segmentaiton caractères
+
+# parcours du dossier 'regions' 
+for i in range(len(os.listdir('Test_folder/regions'))):
+
+    # récupération de l'image
+    img_list = os.listdir(f'Test_folder/regions/region{i+1}')
+    img_nom= f'Test_folder/regions/region{i+1}/region{i+1}.jpg'
+    print(img_nom)
+
+    # Définition de l'image et de sa binarisation
+    img = cv2.imread(img_nom)
+    plt.figure()
+    plt.imshow(img)
+    plt.show()
+    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # (thresh, image_binary) = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+    # On la sépare en lignes et on récupère la 1ère ligne
+    indices_lignes = separe_en_lignes(img)
+
+    indices_debut_fin_ligne = indices_lignes[0]
+
+    #Sur cette permière ligne on sépare les caractères
+    ranges = separe_en_caracteres(img, indices_debut_fin_ligne)
+
+    for elt in ranges :
+
+        # Création du dossier s'il n'existe pas déjà
+        os.makedirs(f'Test_folder/regions/region{i+1}/caract', exist_ok=True)
+
+        # Sauvegarder l'image traitée
+        output_path = f'Test_folder/regions/region{i+1}/caract/caractregion{i+1}.jpg'
+
+        caract = img[indices_debut_fin_ligne[0]:indices_debut_fin_ligne[1], elt[0]:elt[1]]
+        cv2.imwrite(output_path, caract)
+
